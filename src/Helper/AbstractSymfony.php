5250b90251bf435503dedca5a10fbbd0a1edffa7
Nikolay Nikolaev <evrinoma@gmail.com>
2021-12-14 10:29:41 +0300

    increase checking version

--- src/Helper/AbstractSymfony.php
+++ src/Helper/AbstractSymfony.php
@@ -17,7 +17,7 @@ abstract class AbstractSymfony
     {
         preg_match('/^(^\d+.\d+.)/', Kernel::VERSION, $output);
         if (count($output)) {
-            return (int)preg_replace("/[^0-9]/", "", $output[0]) >= 53;
+            return (int)preg_replace("/[^0-9]/", "", $output[0]) >= 54;
         }
 
         return false;
