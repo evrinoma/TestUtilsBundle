579415327be243230494a314c302c55b084a061b
Nikolay Nikolaev <evrinoma@gmail.com>
2022-01-21 19:41:47 +0300

    add abstract layer for fixtures

---
+++ src/Fixtures/AbstractFixture.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Evrinoma\TestUtilsBundle\Fixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+abstract class AbstractFixture extends Fixture
+{
+    protected static array $data = [];
+
+    protected static string $class = '';
+
+    protected static string $splitter =  '_';
+
+    public static function getReferenceName(): string
+    {
+        return (new \ReflectionClass(static::$class))->getShortName().static::$splitter;
+    }
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->create($manager);
+
+        $manager->flush();
+    }
+
+    abstract protected function create(ObjectManager $manager);
+}
