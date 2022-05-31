9bffa2deb233f9c0b42f2a9354670b852bc99e41
Nikolay Nikolaev <evrinoma@gmail.com>
2022-05-31 21:11:13 +0200

    added new value object active

---
+++ src/ValueObject/Common/AbstractActive.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Evrinoma\TestUtilsBundle\ValueObject\Common;
+
+use Evrinoma\TestUtilsBundle\ValueObject\AbstractValueObject;
+use Evrinoma\TestUtilsBundle\ValueObject\ValueObjectTest;
+use Evrinoma\UtilsBundle\Model\ActiveModel;
+
+abstract class AbstractActive extends AbstractValueObject implements ValueObjectTest
+{
+//region SECTION: Fields
+    private const ACTIVE_MODEL_WRONG = 'w';
+//endregion Fields
+
+//region SECTION: Public
+    public static function value(): string
+    {
+        return static::$value ?? ActiveModel::ACTIVE;
+    }
+
+    public static function wrong(): string
+    {
+        return static::ACTIVE_MODEL_WRONG;
+    }
+//endregion Public
+}
